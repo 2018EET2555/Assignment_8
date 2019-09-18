2722ebb432e44119e114d04b2e57ad6dd6fdfc94
2018EET2555 <eet182555@ee.iitd.ac.in>
2019-09-18 10:03:22 +0530

    ps1 completed

--- ps1.py
+++ ps1.py
@@ -3,12 +3,18 @@
 s=raw_input("enter binary bit data that has to be transmitted: ")
 #sting conversion
 s=str(s)
+#count 1 in string
 count_one=s.count("1")
+#if count is even append 1
 if(count_one%2==0):
 	p="1"
 else:
-	p="0"
+	p="0"  
+# append parity
 print("Parity bit data: {s}".format(s=s+p))
+#insert 0 after 010
 s=s.replace("010","0100")
+#append parity  and flag
 s=s+p+"0101"
+#print it
 print('Transmitting data: {p}'.format(p=s))

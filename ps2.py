58cae43b4ae2756e60ac09633a109a25beafd01f
2018EET2555 <eet182555@ee.iitd.ac.in>
2019-09-18 10:25:27 +0530

    ps2 is started half done

--- ps2.py
+++ ps2.py
@@ -1,1 +1,20 @@
+# def check_validity():
 
+# def enter():
+print("Welcome to the Game!")
+def game():
+	print("Player 1's chance")
+	check=1
+	while check==1   : #ask user want to continue
+		p=raw_input("Enter the position and number to be entered: ")
+		print(p)
+		check=0
+
+
+
+arr=[[0 for i in range(3)] for j in range(3)]
+print(game)
+flag="1"
+while flag=="1":
+	game()
+	flag=str(raw_input("enter 1 for continue enter 0 other key for exit "))

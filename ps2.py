0bc6e9a26a58e6a6f6e274b5005b8df6308ac358
2018EET2555 <eet182555@ee.iitd.ac.in>
2019-09-18 11:42:05 +0530

    cpode almost done

--- ps2.py
+++ ps2.py
@@ -2,39 +2,79 @@
 
 # def enter():
 print("Welcome to the Game!")
-def enter(p,v):
+def enter(p,v,player):
+	flag=4
 	i=(v-1)/3
 	j=(v-1)%3
 	arr[i][j]=v
-
-def isvalid(p,val,player):
-	if p>9 || p<1 || (player==1 && v%2==0) || (player=2 && v%2==1) || v>9 ||v<1 || (v in numbers):
+	# sum_r[i][player]+=
+	sum_r[i][0]+=v
+	sum_r[i][1]+=1
+	if sum_r[i][0]==15 && sum_r[i][1]==3:
+		return player
+	sum_c[j][0]+=v
+	sum_c[j][1]+=1
+	if sum_c[j][0]==15 && sum_c[j][1]==3:
+		return player
+	if(i==j):
+			sum_d[0][0]+=v
+			sum_d[0][1]+=1
+	if i+j==2:
+			sum_d[1][0]+=v
+			sum_d[1][1]+=1
+    if (sum_d[0][0]==15 && sum_d[0][1]==3) || (sum_d[1][0]==15 && sum_d[1][1]==3) :
+		return player
+   
+	for k in range(3):
+		if sum_r[k][0]<15 || sum_c[k][0]<15:
+			flag=0
+			return flag
+	for k in range(2):
+		if(sum_d[i][0]<15):
+			flag=0
+			return flag
+	return flag
+def isvalid(p,v,player):
+	if p>9 || p<1 || (player==1 && v%2==0) || (player=2 && v%2==1) || v>9 ||v<1 || (v in num) || (p in pos):
 		return 0
 	return 1 
 
 def game():
 	print("Player 1's chance")
-	check=1     #->>>>>>>>>>>>>>>>>>>>>>>>>>> modifgfy check
+	     #->>>>>>>>>>>>>>>>>>>>>>>>>>> modifgfy check
 	player=1
-	while check==1   : #ask user want to continue
+	while(1)   : #ask user want to continue
+		print("player {play}'s chance".format(play=player))
 		p,v=raw_input("Enter the position and number to be entered: ").split()
 		p=int(p)
 		v=int(v)
 		if isvalid(p,v,player):
-			enter(p,v)
-
-			numbers=[]
-			print(p,v,type(p))
-			check=0
+			dic=enter(p,v,player)
+			num.append(v)
+			pos.append(p)
+			if(dic==4):
+				print("Game Draw")
+				break
+		    if(dic==player):
+		    	print("player {play} wins".format(play))
+		    	break
+			if player==1:
+				player=2
+			else:
+				player=1
 		else:
-			print("value not valid enter again")
+			print("pos or value not valid enter again")
 
 
 
 # print(game)
 flag="1"
 while flag=="1":
-	numbers=[]
+	sum_r=[[0,0] for i in range(3)]
+	sum_c=[[0,0] for i in range(3)] 
+    sum_d=[[0,0] for i in range(2)]
+	num=[]
+	pos=[]
 	arr=[[0 for i in range(3)] for j in range(3)]
 	game()
 	flag=str(raw_input("enter 1 for continue enter 0 other key for exit "))
